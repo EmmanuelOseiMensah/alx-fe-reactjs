6d382ac70df2aa64318ff1898b79304065467cb7
EmmanuelOseiMensah <accaoseimensah4christ32@gmail.com>
2026-02-15 05:50:28 +0000

    set up tailwind css for Building React project

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="text-blue-500 font-bold text-3xl p-10 bg-background">
+      If this is blue and bold, Tailwind is working!
+    </div>
+  );
+}
+
+export default App;
